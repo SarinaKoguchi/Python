79f6d333d4d2ff828f35cb5f968f9a31aec34eed
Sarina Koguchi <s.koguchi@a-force.co.jp>
2020-07-06 18:24:36 +0900

    refs #5:python基礎③課題7修正

--- python3_test7.py
+++ python3_test7.py
@@ -1,23 +1,30 @@
-class Vehicle:
+from abc import ABCMeta, abstractmethod
+
+class Vehicle(metaclass=ABCMeta):
     def __init__(self, name, tool, method):
         self.name = name
         self.tool = tool
         self.method = method
 
+    @abstractmethod
+    def move_on(self):
+        pass
+
+class Car(Vehicle):
     def move_on(self):
         print(f"{self.name}は{self.tool}を{self.method}と進みます。")
 
-class Car(Vehicle):
-    pass
-
 class Bicycle(Vehicle):
-    pass
+    def move_on(self):
+        print(f"{self.name}は{self.tool}を{self.method}と進みます。")
 
 class Ship(Vehicle):
-    pass
+    def move_on(self):
+        print(f"{self.name}は{self.tool}を{self.method}と進みます。")
 
 class Airplane(Vehicle):
-    pass
+    def move_on(self):
+        print(f"{self.name}は{self.tool}を{self.method}と進みます。")
 
 car = Car("車", "アクセル", "踏む")
 bicycle = Bicycle("自転車", "ペダル", "漕ぐ")
