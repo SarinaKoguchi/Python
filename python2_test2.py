57038fe44417887f84315f1a1c4e2941894bfccc
Sarina Koguchi <s.koguchi@a-force.co.jp>
2020-07-08 10:53:11 +0900

    refs #3:閏年判定修正&別解追加

--- python2_test2.py
+++ python2_test2.py
@@ -1,11 +1,11 @@
 import calendar
 import datetime
 
-def check_leap_year(year,date):
+def check_leap_year(year):
     if calendar.isleap(year):
-        print(f"{year}年{date}は閏年です。")
+        return True
     else:
-        print(f"{year}年{date}は閏年ではありません。")
+        return False
 
 today = datetime.date.today()
 last_year = today.year - 1
@@ -14,5 +14,8 @@ date = today.strftime('%m月%d日')
 
 # 昨年、現在、来年の閏年判定
 for judge in range(3):
-    check_leap_year(year, date)
+    if check_leap_year(year):
+         print(f"{year}年{date}は閏年です。")
+    else:
+        print(f"{year}年{date}は閏年ではありません。")
     year += 1
